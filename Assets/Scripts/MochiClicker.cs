5e54599fa2c6c2325a60ce1fabaf2c3c927d842b
Michał Wójtowicz <m.wojtowicz@live.co.uk>
2020-11-03 16:35:38 +0100

    Mochi clicker change color to the beat

--- Assets/Scripts/MochiClicker.cs
+++ Assets/Scripts/MochiClicker.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class MochiClicker : MonoBehaviour
 {
+    private float _timeStampOpen;
+    private float _timeStampClose;
+    public float delayBetweenClicks = 1f;
+
+    SpriteRenderer spriteRenderer;
+    Color activeColor;
+
+    // temp mochi counter
+    int mochiCounter;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.color = Color.white;
+
+        _timeStampOpen = Time.time;
+        _timeStampClose = Time.time + 0.2f;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (_timeStampOpen >= Time.time)
+        {
+            spriteRenderer.color = Color.white;
+        } else if (_timeStampOpen <= Time.time && _timeStampClose >= Time.time)
+        {
+            spriteRenderer.color = Color.red;
+        }
+        else if (_timeStampOpen <= Time.time && _timeStampClose <= Time.time)
+        {
+            _timeStampOpen += delayBetweenClicks;
+            _timeStampClose += delayBetweenClicks;
+        }
     }
 }
