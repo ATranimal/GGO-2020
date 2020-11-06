3dd3e00a074e3439ebec601917b4d48bc63371bf
Michał Wójtowicz <m.wojtowicz@live.co.uk>
2020-11-06 12:54:10 +0100

    Added saving waypoints positions to list

--- Assets/Scripts/RabbitSpawner.cs
+++ Assets/Scripts/RabbitSpawner.cs
@@ -7,20 +7,40 @@ public class RabbitSpawner : MonoBehaviour
     [SerializeField]
     GameObject rabbitPrefab;
     [SerializeField]
-    float frequency;
+    float timeBetweenSpawns;
+
+    List<Vector2> listOfWaypoints = new List<Vector2>();
 
     float _timeUntilNextSpawn;
 
-    // Update is called once per frame
+    private void Start()
+    {
+        for (int waypointNumber = 0; waypointNumber < transform.childCount; waypointNumber++)
+        {
+            var child = transform.GetChild(waypointNumber);
+            Vector2 positionOfWaypoint = ((Vector2)child.position);
+            listOfWaypoints.Add(positionOfWaypoint);
+        }
+            
+
+        PrintWaypoints();
+    }
     void Update()
     {
         bool isTimeToSpawn = _timeUntilNextSpawn <= Time.time;
         if (isTimeToSpawn)
         {
             Instantiate(rabbitPrefab);
-            _timeUntilNextSpawn += frequency;
+
+            _timeUntilNextSpawn += timeBetweenSpawns;
         }
+    }
 
-
+    void PrintWaypoints()
+    {
+        foreach (var point in listOfWaypoints)
+        {
+            Debug.Log(point);
+        }
     }
 }
